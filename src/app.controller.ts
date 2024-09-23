83da268ec3d5e7e467b80bff8cc24672ade375b0
Petrik Diak <diak@petrik.hu>
2024-09-23 14:09:05 +0200

    hdwas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,54 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { title } from 'process';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
-  getHello() {
+  getHello(@Query('bgColor') bgColor: string = "white") {
     return {
+      bgColor: bgColor,
       message: this.appService.getHello()
     };
+  }
+
+  #books = [
+    {
+      title: "qqqqqqqqqqqq",
+      isbn: "1"
+    }, {
+      title: "wwwwwwwwwwwwwwwwww",
+      isbn: "2"
+    }, {
+      title: "eeeeeeeee",
+      isbn: "3"
+    }
+  ]
+
+  @Get('book/:isbn')
+  @Render('book')
+  getBooksByIsbn(@Param('isbn') isbn: string) {
+    return this.#books.find(b => b.isbn == isbn)
+  }
+
+
+  @Get('books')
+  @Render('books')
+  getBooks() {
+    return {
+      message: this.#books
+    }
+  }
+
+
+  @Get('books/:searchkey')
+  @Render('books')
+  getSearchedBooks(@Param('searchkey') searchkey:string) {
+    return {
+      message: this.#books.filter(x=> x.title.indexOf(searchkey))
+    }
   }
 }
